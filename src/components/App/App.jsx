8e7a7e08e582663ec81ca5ce3220a86b311bf4e4
Padee Vang <vangpadee11@gmail.com>
2024-09-02 20:10:21 -0500

    created varaible, useEffect, working on GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  // variables
+
+  const [pizzaArray, setPizzaArray] = useState([]);
+
+  // useEffect
+  useEffect (() => {
+    getPizzas();
+  },[])
+
+  // GET 
+
+  const getPizzas = () => {
+   // console.log('get pizza array');
+
+   axios.get('/api/pizza')
+   .then((response) => {
+    console.log(response.data);
+   })
+
+  }
   return (
     <div className='App'>
       <header className='App-header'>
